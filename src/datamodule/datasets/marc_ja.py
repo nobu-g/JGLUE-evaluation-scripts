85d0c12d3527a077e8f23a5fc6f9525f3e0623a2
nobu-g <ueda-n127@hotmail.co.jp>
2023-03-19 20:00:51 +0900

    fix

--- src/datamodule/datasets/marc_ja.py
+++ src/datamodule/datasets/marc_ja.py
@@ -5,7 +5,6 @@ from typing import Any
 from datasets import Dataset as HFDataset
 from datasets import load_dataset
 from rhoknp import Jumanpp
-from tokenizers import Encoding
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 from transformers.utils import PaddingStrategy
@@ -25,6 +24,7 @@ class MarkJaDataset(Dataset[MarkJaFeatures]):
         split: str,
         tokenizer: PreTrainedTokenizerBase,
         max_seq_length: int,
+        limit_examples: int = -1,
     ) -> None:
         super().__init__()
         self.split: str = split
@@ -35,29 +35,30 @@ class MarkJaDataset(Dataset[MarkJaFeatures]):
         if self.split == "test":
             self.split = "validation"
         dataset = load_dataset("shunk031/JGLUE", name="MARC-ja", split=self.split)
-        # apply Juman++ segmentation
-        dataset = dataset.map(
+        if limit_examples > 0:
+            dataset = dataset.select(range(limit_examples))
+        self.hf_dataset: HFDataset = dataset.map(
             lambda x: {"segmented": batch_segment(x["sentence"])},
             batched=True,
             batch_size=100,
             num_proc=os.cpu_count(),
+        ).map(
+            lambda x: self.tokenizer(
+                x["segmented"],
+                padding=PaddingStrategy.MAX_LENGTH,
+                truncation=True,
+                max_length=self.max_seq_length - 2,  # +2 for [CLS] and [SEP]
+            ),
+            batched=True,
         )
-        self.hf_dataset: HFDataset = dataset
 
     def __getitem__(self, index: int) -> MarkJaFeatures:
         example: dict[str, Any] = self.hf_dataset[index]
-        label: int = example["label"]
-        encoding: Encoding = self.tokenizer(
-            example["segmented"],
-            padding=PaddingStrategy.MAX_LENGTH,
-            truncation=True,
-            max_length=self.max_seq_length - 2,  # +2 for [CLS] and [SEP]
-        ).encodings[0]
         return MarkJaFeatures(
-            input_ids=encoding.ids,
-            attention_mask=encoding.attention_mask,
-            token_type_ids=encoding.type_ids,
-            labels=label,
+            input_ids=example["input_ids"],
+            attention_mask=example["attention_mask"],
+            token_type_ids=example["token_type_ids"],
+            labels=example["label"],
         )
 
     def __len__(self) -> int:
