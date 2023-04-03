f7c19aea7a1582e991ce4112cfe4f42b7c6b77a4
nobu-g <ueda-n127@hotmail.co.jp>
2023-04-03 18:35:39 +0900

    add tests for JSQuAD

--- tests/datasets/test_jsquad.py
+++ tests/datasets/test_jsquad.py
@@ -1,6 +1,6 @@
 from transformers import PreTrainedTokenizerBase
 
-from datamodule.datasets import JsquadDataset
+from datamodule.datasets.jsquad import Answer, JsquadDataset, JsquadExample
 
 
 def test_init(tokenizer: PreTrainedTokenizerBase):
@@ -17,3 +17,34 @@ def test_getitem(tokenizer: PreTrainedTokenizerBase):
         assert len(feature.token_type_ids) == max_seq_length
         assert isinstance(feature.start_positions, int)
         assert isinstance(feature.end_positions, int)
+
+
+def test_features_0(tokenizer: PreTrainedTokenizerBase):
+    max_seq_length: int = 128
+    dataset = JsquadDataset("validation", tokenizer, max_seq_length, limit_examples=1)
+    example = JsquadExample(
+        id="a10336p0q0",
+        title="梅雨",
+        context="梅雨 [SEP] 梅雨 （ つゆ 、 ばいう ） は 、 北海道 と 小笠原 諸島 を 除く 日本 、 朝鮮 半島 南部 、 中国 の 南部 から 長江 流域 に かけて の 沿海 部 、 および 台湾 など 、 東 アジア の 広範囲に おいて み られる 特有の 気象 現象 で 、 5 月 から 7 月 に かけて 来る 曇り や 雨 の 多い 期間 の こと 。 雨季 の 一種 である 。",
+        question="日本 で 梅雨 が ない の は 北海道 と どこ か 。",
+        answers=[
+            Answer(text="小笠原 諸島", start=35),
+            Answer(text="小笠原 諸島 を 除く 日本", start=35),
+            Answer(text="小笠原 諸島", start=35),
+        ],
+        is_impossible=False,
+    )
+    features = dataset[0]
+    question_tokens: list[str] = tokenizer.tokenize(example.question)
+    context_tokens: list[str] = tokenizer.tokenize(example.context)
+    input_tokens = (
+        [tokenizer.cls_token] + question_tokens + [tokenizer.sep_token] + context_tokens + [tokenizer.sep_token]
+    )
+    padded_input_tokens = input_tokens + [tokenizer.pad_token] * (max_seq_length - len(input_tokens))
+    assert features.input_ids == tokenizer.convert_tokens_to_ids(padded_input_tokens)
+    assert features.attention_mask == [1] * len(input_tokens) + [0] * (max_seq_length - len(input_tokens))
+    assert features.token_type_ids == [0] * (len(question_tokens) + 2) + [1] * (len(context_tokens) + 1) + [0] * (
+        max_seq_length - len(input_tokens)
+    )
+    # assert features.start_positions == 35
+    # assert features.end_positions == 41
