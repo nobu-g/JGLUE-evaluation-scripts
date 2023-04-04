18a57cbf322fa3eedaa66cb0973f298af5c2d9b4
nobu-g <ueda-n127@hotmail.co.jp>
2023-04-04 20:20:25 +0900

    add tests for JSQuAD metrics

--- tests/test_jsquad_metric.py
+++ tests/test_jsquad_metric.py
@@ -8,7 +8,7 @@ CASES = [
         ],
         "target": [{"answers": {"answer_start": [97], "text": ["1976"]}, "id": "000"}],
         "exact_match": 1.0,
-        "f1": 1.0,
+        "f1": 1.0,  # precision: 1 / 1, recall: 1 / 1
     },
     {
         "preds": [
@@ -16,7 +16,23 @@ CASES = [
         ],
         "target": [{"answers": {"answer_start": [10], "text": ["2 時間"]}, "id": "001"}],
         "exact_match": 0.0,
-        "f1": 2 / 3,  # precision: 2 / 2, recall: 2 / 4
+        "f1": 2 / 3,  # precision: 2 / 4, recall: 2 / 2
+    },
+    {
+        "preds": [
+            {"prediction_text": "2 時間 21 分", "id": "001"},
+        ],
+        "target": [{"answers": {"answer_start": [10, 10], "text": ["2 時間", "2 時間 21 分"]}, "id": "001"}],
+        "exact_match": 1.0,
+        "f1": 1.0,  # precision: 4 / 4, recall: 4 / 4
+    },
+    {
+        "preds": [
+            {"prediction_text": "2 時間 21 分", "id": "001"},
+        ],
+        "target": [{"answers": {"answer_start": [10, 12], "text": ["2 時間", "時間 21 分"]}, "id": "001"}],
+        "exact_match": 0.0,
+        "f1": 6 / 7,  # precision: 3 / 4, recall: 3 / 3
     },
 ]
 
