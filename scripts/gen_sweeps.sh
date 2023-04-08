324339dcc8ba8ecd43f1069a41dc21bef0e9f4de
nobu-g <ueda-n127@hotmail.co.jp>
2023-04-09 02:57:23 +0900

    tweak

--- scripts/gen_sweeps.sh
+++ scripts/gen_sweeps.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 for task in marc_ja jsts jnli jsquad jcqa; do
   for model in roberta_base roberta_large deberta_base deberta_large; do
     sweep_id=$(wandb sweep --name="${task}-${model}" "sweeps/${task}/${model}.yaml" 2>&1 | tail -1 | cut -d' ' -f8)
-    echo "${task}-${model}" "${sweep_id}" 0 >> sweep_status.txt
+    echo "${task}-${model}" "${sweep_id}" 0 | tee -a sweep_status.txt
   done
 done
